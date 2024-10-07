99d5db3725a640111b5c50e10c06b454055d5615
amalhadila <amalhadila@gmail.com>
2024-10-07 23:54:18 +0300

    FIRIST COMMET

---
+++ lib/core/styles.dart
@@ -0,0 +1,191 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+
+
+
+
+abstract class Textstyle  {
+   static const textStyle12_light =  TextStyle(
+    fontFamily: 'montserrat',
+    fontSize: 12,
+    fontWeight: FontWeight.w300,
+    
+  );
+  static const textStyle12_regular =  TextStyle(
+    fontFamily: 'montserrat',
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    
+  );
+  static const textStyle12_meduim =  TextStyle(
+    fontFamily: 'montserrat',
+    fontSize: 12,
+    fontWeight: FontWeight.w500,
+    
+  );
+  static const textStyle12_semibold =  TextStyle(
+    fontFamily: 'montserrat',
+    fontSize: 12,
+    fontWeight: FontWeight.w600,
+    
+  );
+  static const textStyle12_bold =  TextStyle(
+    fontFamily: 'montserrat',
+    fontSize: 12,
+    fontWeight: FontWeight.w700,
+    
+  );
+  static const textStyle14_light =  TextStyle(
+    fontFamily: 'montserrat',
+    fontSize: 14,
+    fontWeight: FontWeight.w300,
+    
+  );
+  static const textStyle14_regular =  TextStyle(
+    fontFamily: 'montserrat',
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    
+  );
+  static const textStyle14_meduim =  TextStyle(
+    fontFamily: 'montserrat',
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+    
+  );
+  static const textStyle14_semibold =  TextStyle(
+    fontFamily: 'montserrat',
+    fontSize: 14,
+    fontWeight: FontWeight.w600,
+    
+  );
+  static const textStyle14_bold =  TextStyle(
+    fontFamily: 'montserrat',
+    fontSize: 14,
+    fontWeight: FontWeight.w700,
+    
+  );
+  static const textStyle16_light =  TextStyle(
+    fontFamily: 'montserrat',
+    fontSize: 16,
+    fontWeight: FontWeight.w300,
+    
+  );
+  static const textStyle16_regular =  TextStyle(
+    fontFamily: 'montserrat',
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    
+  );
+  static const textStyle16_meduim =  TextStyle(
+    fontFamily: 'montserrat',
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+    
+  );
+  static const textStyle16_semibold =  TextStyle(
+    fontFamily: 'montserrat',
+    fontSize: 16,
+    fontWeight: FontWeight.w600,
+    
+  );
+  static const textStyle16_bold =  TextStyle(
+    fontFamily: 'montserrat',
+    fontSize: 16,
+    fontWeight: FontWeight.w700,
+    
+  );
+  static const textStyle18_light =  TextStyle(
+    fontFamily: 'montserrat',
+    fontSize: 18,
+    fontWeight: FontWeight.w300,
+    
+  );
+  static const textStyle18_regular =  TextStyle(
+    fontFamily: 'montserrat',
+    fontSize: 18,
+    fontWeight: FontWeight.w400,
+    
+  );
+  static const textStyle18_meduim =  TextStyle(
+    fontFamily: 'montserrat',
+    fontSize: 18,
+    fontWeight: FontWeight.w500,
+    
+  );
+  static const textStyle18_semibold =  TextStyle(
+    fontFamily: 'montserrat',
+    fontSize: 18,
+    fontWeight: FontWeight.w600,
+    
+  );
+  static const textStyle18_bold =  TextStyle(
+    fontFamily: 'montserrat',
+    fontSize: 18,
+    fontWeight: FontWeight.w700,
+    
+  );
+
+  static const textStyle20_light =  TextStyle(
+    fontFamily: 'montserrat',
+    fontSize: 20,
+    fontWeight: FontWeight.w300,
+    
+  );
+  static const textStyle20_regular =  TextStyle(
+    fontFamily: 'montserrat',
+    fontSize: 20,
+    fontWeight: FontWeight.w400,
+    
+  );
+  static const textStyle20_meduim =  TextStyle(
+    fontFamily: 'montserrat',
+    fontSize: 20,
+    fontWeight: FontWeight.w500,
+    
+  );
+  static const textStyle20_semibold =  TextStyle(
+    fontFamily: 'montserrat',
+    fontSize: 20,
+    fontWeight: FontWeight.w600,
+    
+  );
+  static const textStyle20_bold =  TextStyle(
+    fontFamily: 'montserrat',
+    fontSize: 20,
+    fontWeight: FontWeight.w700,
+    
+  );
+  static const textStyle22_light =  TextStyle(
+    fontFamily: 'montserrat',
+    fontSize: 22,
+    fontWeight: FontWeight.w300,
+    
+  );
+  static const textStyle22_regular =  TextStyle(
+    fontFamily: 'montserrat',
+    fontSize: 22,
+    fontWeight: FontWeight.w400,
+    
+  );
+  static const textStyle22_meduim =  TextStyle(
+    fontFamily: 'montserrat',
+    fontSize: 22,
+    fontWeight: FontWeight.w500,
+    
+  );
+  static const textStyle22_semibold =  TextStyle(
+    fontFamily: 'montserrat',
+    fontSize: 22,
+    fontWeight: FontWeight.w600,
+    
+  );
+  static const textStyle22_bold =  TextStyle(
+    fontFamily: 'montserrat',
+    fontSize: 22,
+    fontWeight: FontWeight.w700,
+    
+  );
+
+}
